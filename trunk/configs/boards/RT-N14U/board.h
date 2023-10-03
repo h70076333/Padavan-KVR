d3855160e328b3e432b1d82be527600907810b87
h70076222 <142376079+h70076222@users.noreply.github.com>
2023-10-04 00:44:17 +0800

    Update board.h

--- trunk/configs/boards/RT-N14U/board.h
+++ trunk/configs/boards/RT-N14U/board.h
@@ -1,21 +1,28 @@
-/* YOUHUA WR1200JS */
+/* ASUS RT-N14U */
 
-#define BOARD_PID		"WR1200JS"
-#define BOARD_NAME		"WR1200JS"
-#define BOARD_DESC		"YOUHUA WR1200JS Wireless Router"
-#define BOARD_VENDOR_NAME	"Youhua Technology"
-#define BOARD_VENDOR_URL	"http://www.youhuatech.com/"
-#define BOARD_MODEL_URL		"http://www.youhuatech.com/"
-#define BOARD_BOOT_TIME		30
+#define BOARD_PID		"RT-N14U"
+#define BOARD_NAME		"RT-N14U"
+#define BOARD_DESC		"ASUS RT-N14U Wireless Router"
+#define BOARD_VENDOR_NAME	"ASUSTek Computer Inc."
+#define BOARD_VENDOR_URL	"http://www.asus.com/"
+#define BOARD_MODEL_URL		"http://www.asus.com/Networking/RTN14U/"
+#define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
-
-#define BOARD_HAS_5G_11AC		1
-#define BOARD_NUM_ANT_5G_TX		2
-#define BOARD_NUM_ANT_5G_RX		2
-#define BOARD_NUM_ANT_2G_TX		2
-#define BOARD_NUM_ANT_2G_RX		2
-#define BOARD_HAS_EPHY_L1000	1
-#define BOARD_HAS_EPHY_W1000	1
-#define BOARD_NUM_ETH_LEDS		0
-#define BOARD_NUM_UPHY_USB3		1
-#define BOARD_USB_PORT_SWAP		0
+#define BOARD_GPIO_BTN_RESET	1
+#define BOARD_GPIO_BTN_WPS	2
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI	72
+#define BOARD_GPIO_LED_POWER	43
+#define BOARD_GPIO_LED_LAN	41
+#define BOARD_GPIO_LED_WAN	40
+#define BOARD_GPIO_LED_USB	42
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	0
+#define BOARD_NUM_ANT_2G_RX	0
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
