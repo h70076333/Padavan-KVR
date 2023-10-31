904ff92cd2b2eb896c7a979e768467216fdc16d7
h70076222 <142376079+h70076222@users.noreply.github.com>
2023-11-01 00:58:41 +0800

    Update dev_init.sh

--- trunk/user/scripts/dev_init.sh
+++ trunk/user/scripts/dev_init.sh
@@ -5,8 +5,8 @@ mount -t sysfs sysfs /sys
 [ -d /proc/bus/usb ] && mount -t usbfs usbfs /proc/bus/usb
 
 size_tmp="24M"
-size_var="4M"
-size_etc="6M"
+size_var="3M"
+size_etc="4M"
 
 if [ "$1" == "-l" ] ; then
 	size_tmp="8M"
@@ -14,7 +14,7 @@ if [ "$1" == "-l" ] ; then
 fi
 
 mount -t tmpfs tmpfs /dev   -o size=8K
-mount -t tmpfs tmpfs /etc   -o size=6M,noatime
+mount -t tmpfs tmpfs /etc   -o size=4M,noatime
 mount -t tmpfs tmpfs /home  -o size=1M
 mount -t tmpfs tmpfs /media -o size=8K
 mount -t tmpfs tmpfs /mnt   -o size=8K
