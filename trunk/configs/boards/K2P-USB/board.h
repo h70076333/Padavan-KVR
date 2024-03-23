53173c75e319970482c889c919b19bbe55b0dda8
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-03-23 20:48:35 +0800

    Update board.h

--- trunk/configs/boards/K2P-USB/board.h
+++ trunk/configs/boards/K2P-USB/board.h
@@ -6,18 +6,18 @@
 #define BOARD_VENDOR_NAME	"PHICOMM"
 #define BOARD_VENDOR_URL	"http://www.phicomm.com/"
 #define BOARD_MODEL_URL		"http://www.phicomm.com/"
-#define BOARD_BOOT_TIME		30
+#define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
 #undef BOARD_GPIO_BTN_FN1
-#undef BOARD_GPIO_BTN_RESET
-#define BOARD_GPIO_BTN_WPS	3
+#undef BOARD_GPIO_BTN_RESET   18
+#define BOARD_GPIO_BTN_WPS	
 #undef  BOARD_GPIO_LED_ALL
-#define  BOARD_GPIO_LED_WIFI	14
+#define  BOARD_GPIO_LED_WIFI	
 #undef  BOARD_GPIO_LED_SW2G
 #undef  BOARD_GPIO_LED_SW5G
-#define  BOARD_GPIO_LED_POWER	13
+#define  BOARD_GPIO_LED_POWER	12	/* 6: red, 10: yellow, 8: blue */
 #undef  BOARD_GPIO_LED_LAN
-#define BOARD_GPIO_LED_WAN	15
+#define BOARD_GPIO_LED_WAN	8
 #undef BOARD_GPIO_LED_USB
 #undef BOARD_GPIO_LED_ROUTER
 #undef  BOARD_GPIO_PWR_USB
@@ -27,6 +27,7 @@
 #define BOARD_NUM_ANT_2G_TX	2
 #define BOARD_NUM_ANT_2G_RX	2
 #define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	4
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
 #define BOARD_NUM_UPHY_USB3	1
