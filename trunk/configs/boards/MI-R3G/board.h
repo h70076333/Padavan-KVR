fea077632649e458a3b30c911be959df3ab99e22
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-07-07 19:39:02 +0800

    Update board.h

--- trunk/configs/boards/MI-R3G/board.h
+++ trunk/configs/boards/MI-R3G/board.h
@@ -1,31 +1,32 @@
 /* Xiaomi Mi R3G */
 
-#define BOARD_PID		"MI-R3G"
-#define BOARD_NAME		"MI-R3G"
-#define BOARD_DESC		"Xiaomi Mi R3G Wireless Router"
-#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
-#define BOARD_VENDOR_URL	"http://www.mi.com/"
-#define BOARD_MODEL_URL		"http://www.mi.com/miwifir3g"
+#define BOARD_PID		"TX180X"
+#define BOARD_NAME		"TX180X"
+#define BOARD_DESC		"TX180X Wireless Router"
+#define BOARD_VENDOR_NAME	"H3C"
+#define BOARD_VENDOR_URL	"http://www.h3c.com.cn/cn/"
+#define BOARD_MODEL_URL		"http://www.h3c.com.cn/cn/"
 #define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
-#define BOARD_GPIO_BTN_RESET	18
-#undef  BOARD_GPIO_BTN_WPS
+#define BOARD_GPIO_BTN_RESET	14
+#define  BOARD_GPIO_BTN_WPS		13
 #undef  BOARD_GPIO_LED_ALL
 #undef  BOARD_GPIO_LED_WIFI
-#define BOARD_GPIO_LED_POWER	8	/* 6: red, 10: yellow, 8: blue */
-#define BOARD_GPIO_LED_LAN	10
 #undef  BOARD_GPIO_LED_WAN
-#define BOARD_GPIO_LED_USB	6
+#define BOARD_GPIO_LED_POWER	16		/* sys_blue: 16, sys_yellow: 15 */
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_USB
 #undef  BOARD_GPIO_LED_ROUTER
-#define BOARD_GPIO_PWR_USB_ON	1	/* 1: 5V Power ON, 0: 5V Power OFF */
-#define BOARD_GPIO_PWR_USB	12	/* USB2.0 5V Power */
+#undef BOARD_GPIO_PWR_USB_ON
+#undef BOARD_GPIO_PWR_USB
 #define BOARD_HAS_5G_11AC	1
+#define BOARD_HAS_5G_11AX	1
+#define BOARD_HAS_2G_11AX	1
 #define BOARD_NUM_ANT_5G_TX	2
 #define BOARD_NUM_ANT_5G_RX	2
 #define BOARD_NUM_ANT_2G_TX	2
 #define BOARD_NUM_ANT_2G_RX	2
-#define BOARD_NUM_ETH_LEDS	1
-#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	4
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
-#define BOARD_NUM_UPHY_USB3	1
