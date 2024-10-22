caf092ca3202db9e610180faab857ef86a2fc10a
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-10-22 20:27:44 +0800

    Update board.h

--- trunk/configs/boards/RG MA1210SDWAN/board.h
+++ trunk/configs/boards/RG MA1210SDWAN/board.h
@@ -1,8 +1,8 @@
 /* PSG1208 */
 
-#define BOARD_PID		"PSG1208"
-#define BOARD_NAME		"PSG1208"
-#define BOARD_DESC		"PHICOMM PSG1208 Wireless Router"
+#define BOARD_PID		"MA1210SDWAN"
+#define BOARD_NAME		"MA1210SDWAN"
+#define BOARD_DESC		"RG MA1210SDWAN Wireless Router"
 #define BOARD_VENDOR_NAME	"PHICOMM"
 #define BOARD_VENDOR_URL	"http://www.phicomm.com/"
 #define BOARD_MODEL_URL		"http://www.phicomm.com/"
