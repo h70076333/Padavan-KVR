602bc9be43fb3d276e59fd70c07014aa8b24987f
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-10-23 01:21:47 +0800

    Update board.mk

--- trunk/configs/boards/RG-BCR810W/board.mk
+++ trunk/configs/boards/RG-BCR810W/board.mk
@@ -4,5 +4,5 @@
 # MI-MINI   # Xiaomi Mi Mini   # MT7620  #
 ##################################################################
 
-CFLAGS += -DBOARD_MIMINI
+CFLAGS += -DBOARD_RG-BCR810W
 BOARD_NUM_USB_PORTS=1
