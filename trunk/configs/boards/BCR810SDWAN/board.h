7fb87723bdc46afeb6b8569f83e91908f3eaccb2
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-10-25 01:36:18 +0800

    Update board.h

--- trunk/configs/boards/BCR810SDWAN/board.h
+++ trunk/configs/boards/BCR810SDWAN/board.h
@@ -2,9 +2,9 @@
 
 #define BOARD_PID		"BCR810SDWAN"
 #define BOARD_NAME		"BCR810SDWAN"
-#define BOARD_VENDOR_NAME	"PHICOMM"
-#define BOARD_VENDOR_URL	"http://www.phicomm.com/"
-#define BOARD_MODEL_URL		"http://www.phicomm.com/"
+#define BOARD_DESC		"RUIJIE Wireless Router"
+#define BOARD_VENDOR_URL	"http://www.ruijiery.com/"
+#define BOARD_MODEL_URL		"http://www.ruijiery.com/"
 #define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
 #define BOARD_GPIO_BTN_RESET	1
