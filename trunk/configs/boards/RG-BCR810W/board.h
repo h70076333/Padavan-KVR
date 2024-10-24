e0eb920ae9aa6e5dec7e769439e03ccc1c044552
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-10-24 11:15:25 +0800

    Update board.h

--- trunk/configs/boards/RG-BCR810W/board.h
+++ trunk/configs/boards/RG-BCR810W/board.h
@@ -3,8 +3,8 @@
 #define BOARD_PID		"RG-BCR810W"
 #define BOARD_NAME		"RG-BCR810W"
 #define BOARD_VENDOR_NAME	"RUIJIE"
-#define BOARD_VENDOR_URL	"http://www.ruijie.com/"
-#define BOARD_MODEL_URL		"http://www.ruijie.com/"
+#define BOARD_VENDOR_URL	"http://www.oyewifi.com"
+#define BOARD_MODEL_URL		"http://www.oyewifi.com"
 #define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
 #undef BOARD_GPIO_BTN_RESET
@@ -20,8 +20,8 @@
 #define BOARD_HAS_5G_11AC	0
 #define BOARD_NUM_ANT_5G_TX	0
 #define BOARD_NUM_ANT_5G_RX	0
-#define BOARD_NUM_ANT_2G_TX	2
-#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ANT_2G_TX	1
+#define BOARD_NUM_ANT_2G_RX	1
 #define BOARD_NUM_ETH_LEDS	1
 #define BOARD_HAS_EPHY_L1000	0
 #define BOARD_HAS_EPHY_W1000	0
