964e29e40cf6292c13cebe5cd922ac3cbda26cbf
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-12-01 16:34:56 +0800

    Update board.h

--- trunk/configs/boards/NX18N/board.h
+++ trunk/configs/boards/NX18N/board.h
@@ -8,14 +8,25 @@
 #define BOARD_MODEL_URL		"http://www.h3c.com.cn/cn/"
 #define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
-
+#define BOARD_GPIO_BTN_RESET	14
+#define  BOARD_GPIO_BTN_WPS		13
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_WAN    16
+#define BOARD_GPIO_LED_POWER	15		/* sys_blue: 15, sys_yellow: 15 */
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef BOARD_GPIO_PWR_USB_ON
+#undef BOARD_GPIO_PWR_USB
 #define BOARD_HAS_5G_11AC	0
+#define BOARD_HAS_5G_11AX	0
+#define BOARD_HAS_2G_11AX	0
 #define BOARD_NUM_ANT_5G_TX	0
 #define BOARD_NUM_ANT_5G_RX	0
 #define BOARD_NUM_ANT_2G_TX	0
 #define BOARD_NUM_ANT_2G_RX	0
 #define BOARD_NUM_ETH_LEDS	0
-#define BOARD_NUM_ETH_EPHY	0
+#define BOARD_NUM_ETH_EPHY	4
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
-#define BOARD_NUM_UPHY_USB3	0
