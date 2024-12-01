ce043ae47362b35117051a2f431e011b01b4e4cb
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-12-01 16:27:53 +0800

    Create board.h

---
+++ trunk/configs/boards/NX18N/board.h
@@ -0,0 +1,21 @@
+/* NX18N */
+
+#define BOARD_PID		"NX18N"
+#define BOARD_NAME		"NX18N"
+#define BOARD_DESC		"NX18N Wireless Router"
+#define BOARD_VENDOR_NAME	"H3C"
+#define BOARD_VENDOR_URL	"http://www.h3c.com.cn/cn/"
+#define BOARD_MODEL_URL		"http://www.h3c.com.cn/cn/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	0
+#define BOARD_NUM_ANT_2G_RX	0
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	0
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	0
