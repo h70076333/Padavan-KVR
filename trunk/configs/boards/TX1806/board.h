e77f3e5bec9a3c857f264f26642889834dbc227b
h70076222 <142376079+h70076222@users.noreply.github.com>
2024-12-02 13:34:32 +0800

    Update board.h

--- trunk/configs/boards/TX1806/board.h
+++ trunk/configs/boards/TX1806/board.h
@@ -1,18 +1,19 @@
+/* TX1806 */
 
-#define BOARD_PID		"CR660x"
-#define BOARD_NAME		"CR660x"
-#define BOARD_DESC		"CR660x Wireless Router"
-#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
-#define BOARD_VENDOR_URL	"http://www.mi.com/"
-#define BOARD_MODEL_URL		"http://www.mi.com/"
+#define BOARD_PID		"TX1806"
+#define BOARD_NAME		"TX1806"
+#define BOARD_DESC		"TX1806 Wireless Router"
+#define BOARD_VENDOR_NAME	"H3C"
+#define BOARD_VENDOR_URL	"http://www.h3c.com.cn/cn/"
+#define BOARD_MODEL_URL		"http://www.h3c.com.cn/cn/"
 #define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
-#define BOARD_GPIO_BTN_RESET	18
-#define  BOARD_GPIO_BTN_WPS		7
+#define BOARD_GPIO_BTN_RESET	14
+#define  BOARD_GPIO_BTN_WPS	13
 #undef  BOARD_GPIO_LED_ALL
 #undef  BOARD_GPIO_LED_WIFI
-#define  BOARD_GPIO_LED_WAN		15		/* net_blue: 15, net_yellow: 13 */
-#define BOARD_GPIO_LED_POWER	16		/* sys_blue: 16, sys_yellow: 14 */
+#define  BOARD_GPIO_LED_WAN	16
+#define BOARD_GPIO_LED_POWER	15
 #undef  BOARD_GPIO_LED_LAN
 #undef  BOARD_GPIO_LED_USB
 #undef  BOARD_GPIO_LED_ROUTER
