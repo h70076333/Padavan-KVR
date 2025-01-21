c456bf076ce71f783bbca27802043f9d92b51282
h70076222 <142376079+h70076222@users.noreply.github.com>
2025-01-21 16:49:43 +0800

    Update board.h

--- trunk/configs/boards/NX18N/board.h
+++ trunk/configs/boards/NX18N/board.h
@@ -6,26 +6,26 @@
 #define BOARD_VENDOR_NAME	"H3C"
 #define BOARD_VENDOR_URL	"http://www.h3c.com.cn/cn/"
 #define BOARD_MODEL_URL		"http://www.h3c.com.cn/cn/"
-#define BOARD_BOOT_TIME		30
+#define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
-#define BOARD_GPIO_BTN_RESET	18
-#define  BOARD_GPIO_BTN_WPS		7
+#define BOARD_GPIO_BTN_RESET	14
+#define  BOARD_GPIO_BTN_WPS		13
 #undef  BOARD_GPIO_LED_ALL
 #undef  BOARD_GPIO_LED_WIFI
-#define  BOARD_GPIO_LED_WAN		16
-#define BOARD_GPIO_LED_POWER	15
+#undef  BOARD_GPIO_LED_WAN
+#define BOARD_GPIO_LED_POWER	16		/* sys_blue: 16, sys_yellow: 15 */
 #undef  BOARD_GPIO_LED_LAN
 #undef  BOARD_GPIO_LED_USB
 #undef  BOARD_GPIO_LED_ROUTER
 #undef BOARD_GPIO_PWR_USB_ON
 #undef BOARD_GPIO_PWR_USB
-#define BOARD_HAS_5G_11AC	0
-#define BOARD_HAS_5G_11AX	0
-#define BOARD_HAS_2G_11AX	0
-#define BOARD_NUM_ANT_5G_TX	0
-#define BOARD_NUM_ANT_5G_RX	0
-#define BOARD_NUM_ANT_2G_TX	0
-#define BOARD_NUM_ANT_2G_RX	0
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_HAS_5G_11AX	1
+#define BOARD_HAS_2G_11AX	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
 #define BOARD_NUM_ETH_LEDS	0
 #define BOARD_NUM_ETH_EPHY	4
 #define BOARD_HAS_EPHY_L1000	1
