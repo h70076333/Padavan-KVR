68db9641a7a337577e184195327f69e88413131a
h70076222 <142376079+h70076222@users.noreply.github.com>
2025-02-24 12:00:30 +0800

    Update afycx.sh

--- trunk/user/hxsdwan/afycx.sh
+++ trunk/user/hxsdwan/afycx.sh
@@ -8,7 +8,7 @@ echo $zero_afykey
 zero_afynen=$(nvram get zero_afynen) 
 echo $zero_afynen
 
-/usr/bin/stdoutsubc $zero_afynen --clientid $zero_afykey --qos 1 --showtopics on --script /usr/bin/bafa.sh  &
+/usr/bin/stdoutsubc $zero_afynen --clientid $zero_afykey --qos 1 --script /usr/bin/bafa.sh  &
 
 sleep 4
 if [ ! -z "`pidof stdoutsubc`" ] ; then
