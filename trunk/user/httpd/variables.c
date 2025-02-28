1e2ae7c90c251b1b2d8adf73a7c70b318a23ef3f
h70076333 <h70076222@88.com>
2025-02-28 17:17:44 +0800

    Update variables.c

--- trunk/user/httpd/variables.c
+++ trunk/user/httpd/variables.c
@@ -1010,6 +1010,8 @@
 			{"zerotiermoon_ip", "", NULL, EVM_RESTART_ZEROTIER},
 			{"zerotier_nat", "", NULL, EVM_RESTART_ZEROTIER},
 			{"zero_staticnum_x", "", NULL, EVM_RESTART_ZEROTIER},
+			{"afycx_enable", "", NULL, EVM_RESTART_ZEROTIER},
+			{"gecoac_enable", "", NULL, EVM_RESTART_ZEROTIER},
 			{"ZeroList", "Group", ARGV((char*)variables_ZeroConf_ZeroList, "8", "55", "zero_staticnum_x"), EVM_RESTART_ZEROTIER},
 			{0,0,0,0}
 	};
