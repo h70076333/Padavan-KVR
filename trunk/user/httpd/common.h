77769593e17386cc5072ff04ab8e81c6409c8a04
h70076111 <h70076222@2980.com>
2025-08-06 16:54:41 +0800

    Update common.h

--- trunk/user/httpd/common.h
+++ trunk/user/httpd/common.h
@@ -174,7 +174,7 @@ typedef u_int8_t u8;
 #define EVT_RESTART_ADGUARDHOME     1
 #define EVT_RESTART_WYY      		2
 #define EVT_RESTART_ZEROTIER     	2
-#define EVT_RESTART_NVPPROXY     	2
+#define EVT_RESTART_ETINK      		2
 #define EVT_RESTART_NELINK      	2
 #define EVT_RESTART_ALDRIVER     	2
 #define EVT_RESTART_WIREGUARD     	2
